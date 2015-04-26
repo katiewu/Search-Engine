38f4ba950d1a2a8190eec7b3ac9efcb8f63e8184
Wu Jingyuan <katiewu0304@gmail.com>
2015-04-26 14:41:45 -0400

    add servlet

--- src/SearchInterface/SearchServlet.java
+++ src/SearchInterface/SearchServlet.java
@@ -1,9 +1,9 @@
 package SearchInterface;
 
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import javax.servlet.ServletException;
@@ -16,6 +16,7 @@ import snowballstemmer.PorterStemmer;
 /**
  * Servlet implementation class SearchInterface
  */
+
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String PARSER = " \t\n\r";
@@ -24,22 +25,43 @@ public class SearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	
-	public static String stemContent(String content) {
+	public static List<String> stemContent(String content) {
 		StringTokenizer tokenizer = new StringTokenizer(content, PARSER);
 		String word = "";
 		PorterStemmer stemmer = new PorterStemmer();
-		StringBuilder sb = new StringBuilder();
+		List<String> parseQuery = new ArrayList<String>();
 		while (tokenizer.hasMoreTokens()) {
 			word = tokenizer.nextToken(); 
 			if(word.equals("")) continue;
+			boolean flag = false;
+			for(int i=0;i<word.length();i++){
+				if (Character.UnicodeBlock.of(word.charAt(i)) != Character.UnicodeBlock.BASIC_LATIN) {
+					flag = true;
+					break;
+				}
+			}	
+			if(flag) continue;
+			int i = 0;
+			while(i<word.length() && ( !Character.isLetter(word.charAt(i)) && !Character.isDigit(word.charAt(i)) )){
+				i++;
+			}
+			if(i>=word.length()) continue;
+			word = word.substring(i);
+			i = word.length()-1;
+			while(i>=0 && ( !Character.isLetter(word.charAt(i)) && !Character.isDigit(word.charAt(i)) )){
+				i--;
+			}
+			if(i<0) continue;
+			word = word.substring(0, i+1);
 			stemmer.setCurrent(word);
 			if(stemmer.stem()){
-				sb.append(stemmer.getCurrent());
-				sb.append(" ");
+				parseQuery.add(word);
 			}
 		}
-		return new String(sb);
+		return parseQuery;
 	}
+	
+	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
@@ -48,6 +70,7 @@ public class SearchServlet extends HttpServlet {
 		out.println("<input type=\"text\" name=\"search\">");
 		out.println("<br><input type=\"submit\" value=\"Search\">");
 		out.println("</form>");
+		out.println("</body></html>");
 	}
 
 	/**
@@ -55,7 +78,18 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		String query = request.getParameter("search");
+		String query = request.getParameter("search").toLowerCase();
+		List<String> parseQuery = stemContent(query);
+		/*
+		 * search query
+		 */
+		out.println("<html><body>");
+		out.println("<h1>Result</h1>");
+		for(String w:parseQuery){
+			out.println("<li>"+w+"</li>");
+		}
+		out.println("</body></html>");
+		
 		
 	}
 
--- src/indexer/WordCountMapper.java
+++ src/indexer/WordCountMapper.java
@@ -1,7 +1,6 @@
 package indexer;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
@@ -16,48 +15,18 @@ public class WordCountMapper extends Mapper<Text, Text, Text, IntWritable> {
 
 	private static final String PARSER = " \t\n\r";
 	private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
     
     public static String html2text(String content) {
 	    return Jsoup.parse(content).text();
 	}
 	
-	public static String stemContent(String content) {
+    public static String stemContent(String content) {
 		StringTokenizer tokenizer = new StringTokenizer(content, PARSER);
 		String word = "";
 		PorterStemmer stemmer = new PorterStemmer();
 		StringBuilder sb = new StringBuilder();
 		while (tokenizer.hasMoreTokens()) {
 			word = tokenizer.nextToken(); 
-			if(word.equals("")) continue;
-			stemmer.setCurrent(word);
-			if(stemmer.stem()){
-				sb.append(stemmer.getCurrent());
-				sb.append(" ");
-			}
-		}
-		return new String(sb);
-	}
-    
-	public void map(Text key, Text value, Context context) {
-		// key: docID, value: content
-		String docID = key.toString();
-		String[] val = value.toString().split("\n");
-		if(val.length != 2) return;
-		String content = val[1].toLowerCase();
-		content = html2text(content);
-
-		// stemming the content
-		content = stemContent(content);
-		
-		// process one document
-		HashMap<String, WordInfo> wordSet = new HashMap<String, WordInfo>();
-		int position = 0;
-		StringTokenizer tokenizer = new StringTokenizer(content, PARSER);
-		String word = "";
-		int max = 0;
-		while (tokenizer.hasMoreTokens()) {
-			word = tokenizer.nextToken();
 			if(word.equals("")) continue;
 			boolean flag = false;
 			for(int i=0;i<word.length();i++){
@@ -80,22 +49,48 @@ public class WordCountMapper extends Mapper<Text, Text, Text, IntWritable> {
 			if(i<0) continue;
 			word = word.substring(0, i+1);
 			System.out.println(word);
-			if(!wordSet.containsKey(word)){
-				WordInfo info = new WordInfo();
-				wordSet.put(word, info);
+			stemmer.setCurrent(word);
+			if(stemmer.stem()){
+				sb.append(stemmer.getCurrent());
+				sb.append(" ");
 			}
-			wordSet.get(word).addPosition(position);
-			int size = wordSet.get(word).getSize();
-			if(size>max) max = size;
-			position++;
 		}
-		for(String w:wordSet.keySet()){
+		return new String(sb);
+	}
+    
+	public void map(Text key, Text value, Context context) {
+		// key: docID, value: content
+		String[] val = value.toString().split("\n");
+		if(val.length != 2) return;
+		String content = val[1].toLowerCase();
+		content = html2text(content);
+
+		// stemming the content
+		content = stemContent(content);
+		
+		// process one document
+		HashSet<String> wordSet = new HashSet<String>();
+		StringTokenizer tokenizer = new StringTokenizer(content, PARSER);
+		String word = "";
+		while (tokenizer.hasMoreTokens()) {
+			word = tokenizer.nextToken();
+			if(word.equals("")) continue;
+			if(!wordSet.contains(word)){
+				wordSet.add(word);
+			}
+		}
+		for(String w:wordSet){
 			try {
 				context.write(new Text(w), one);
 			} catch (IOException | InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public static void main(String[] args){
+		String content = "jdiao djiao,djaioej eo 'djeifj' jfei =fjei";
+		System.out.println(stemContent(content));
 	}
 
 }
